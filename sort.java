57200300a5d12d3aea9eab456bfb46c4eb45f85a
Garv <garvsharma40@gmial.com>
2024-07-10 12:58:14 +0530

    commit

---
+++ sort.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class sort{
+    static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static sorting(int arr)
+}
