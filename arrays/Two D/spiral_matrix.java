6899a92e0ef9ccf6977a7ad8edddabaf563c079c
Garv <garvsharma40@gmial.com>
2024-07-18 19:28:06 +0530

    commit

--- arrays/Two D/spiral_matrix.java
+++ arrays/Two D/spiral_matrix.java
@@ -1,41 +1,53 @@
+import java.util.*;
 public class spiral_matrix 
 {
     public static void main(String args[])
     {
-        int mat[][] = {{1,2,3},{4,5,6},{7,8,10}};
-        int r = mat.length, c = mat[0].length;
-        int topRow=0, bottomRow=r-1, leftCol=0, rightCol=c-1;
-        int totalEle=0;
+        Scanner ob = new Scanner(System.in);
+        System.out.println("Enter Size of the matrix");
+        int r = ob.nextInt();
+        int c = ob.nextInt();
+        int mat[][] = new int[r][c];
+        for (int i = 0; i < r; i++) 
+        {
+            for (int j = 0; j < c; j++) 
+            {
+                mat[i][j] = ob.nextInt();   
+            }    
+        }
 
+        int leftCol=0, topRow=0, rightCol=c-1, bottomRow=r-1;
+        int totalEle = 0;
+        
         while(totalEle < r*c)
         {
-            for(int i = leftCol; i <= rightCol && totalEle < r*c ; i++)
+            for(int i = leftCol; i <= rightCol && totalEle < r*c; i++)
             {
-                System.out.print(mat[topRow][i]);
+                System.out.print(mat[topRow][i] + " ");
                 totalEle++;
             }
             topRow++;
 
-            for(int i = topRow; i <= bottomRow && totalEle < r*c ; i++)
+            for(int i = topRow; i <= bottomRow && totalEle < r*c; i++)
             {
-                System.out.print(mat[i][rightCol]);
+                System.out.print(mat[i][rightCol] + " ");
                 totalEle++;
             }
             rightCol--;
 
-            for(int i = rightCol; i >= leftCol && totalEle < r*c ; i--)
+            for(int i = rightCol; i >= leftCol && totalEle < r*c; i--)
             {
-                System.out.print(mat[bottomRow][i]);
+                System.out.print(mat[bottomRow][i] + " ");
                 totalEle++;
             }
             bottomRow--;
 
-            for(int i = bottomRow; i >= topRow && totalEle < r*c ; i--)
+            for(int i = bottomRow; i >= topRow && totalEle < r*c; i--)
             {
-                System.out.print(mat[i][leftCol]);
+                System.out.print(mat[i][leftCol] + " ");
                 totalEle++;
             }
-            leftCol--;
+            leftCol++;
         }
     }
 }
