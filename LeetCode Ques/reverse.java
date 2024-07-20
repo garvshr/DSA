3089c79dcce3159a7b4b69591f1deaee0ebc88dd
Garv <garvsharma40@gmial.com>
2024-07-20 16:22:52 +0530

    commit

--- LeetCode Ques/reverse.java
+++ LeetCode Ques/reverse.java
@@ -6,7 +6,7 @@ public class reverse {
         int rev=0;
         while(n !=0 )
         {
-            if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) return 0;
+            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10) return 0;
             int d = n % 10;
             n /= 10;
             rev = (rev*10) + d;
